9e05453ce502f3101568840592b5d583d72fff79
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-30 19:50:50 +0300

    overrided the Markdown img tag

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 import PostHeader from "./post-header";
 
 import classes from "./post-content.module.css";
@@ -7,10 +8,24 @@ function PostContent(props) {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderes = {
+    image(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    }
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderes}>{post.content}</ReactMarkdown>
     </article>
   );
 }
