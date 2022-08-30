dfd637412fed3d87956f5f2ec26d91b9e8a1a441
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-30 17:08:50 +0300

    rendering some Dummy Post Data and some styling changes

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -13,15 +13,22 @@ function PostItem(props) {
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   return (
     <li className={classes.post}>
-      <Link>
+      <Link href={linkPath}>
         <a>
-          <div className={classes.content}>
-            <Image src={imagePath} alt={title} width={300} height={200} />
+          <div className={classes.image}>
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              layout="responsive"
+            />
           </div>
-          <div>
+          <div className={classes.content}>
             <h3>{title}</h3>
             <time>{formattedDate}</time>
             <p>{excerpt}</p>
