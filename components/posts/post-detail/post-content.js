e6e229a79ac0d7c8d4091cfb534fe7138239b479
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-30 19:55:36 +0300

    overrided the paragraph tag, avoiding rendering the Image inside a <p> tag

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -10,16 +10,26 @@ function PostContent(props) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderes = {
-    image(image) {
-      return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-          width={600}
-          height={300}
-        />
-      );
-    }
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
   };
 
   return (
